f17e98817f83156cec8cf28a294ba750ff944d34
sharapovk <sharapovk@gmail.com>
2022-02-20 14:42:45 +0300

    feat: add brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+function check(str, bracketsConfig) {
+  const stack = [];
+
+  for (const bracket of str) {
+    const config = bracketsConfig.filter((config) =>
+      config.includes(bracket)
+    )[0];
+
+    if (bracket === config[1] && stack[stack.length - 1] === config[0]) {
+      stack.pop();
+    } else if (bracket === config[0]) {
+      stack.push(bracket);
+    } else {
+      return false;
+    }
+  }
+
+  return !(stack.length > 0);
 }
+
+module.exports = check;
